0bb3d38bf716cbb7a1ca7627f11f7a5d3122d8e1
ktkk <62206630+ktkk@users.noreply.github.com>
2020-10-20 20:38:29 +0200

    Update bookmarks.js

--- bookmarks.js
+++ bookmarks.js
@@ -12,26 +12,17 @@ const bookmarks = [
     title: "Media",
     links: [
       { name: "Youtube", url: "https://youtube.com" },
+	  { name: "Twitch", url: "https://www.twitch.tv/" },
       { name: "Netflix", url: "https://netflix.com" },
-      { name: "Crunchyroll", url: "https://crunchyroll.com" },
-      {
-        name: "Amazon Prime",
-        url: "https://www.amazon.com/Amazon-Video",
-      },
     ],
   },
   {
     title: "Reddit",
     links: [
-      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
+      { name: "/r/all", url: "https://www.reddit.com/r/all/" },
       {
         name: "/r/pcmasterrace",
         url: "https://reddit.com/r/pcmasterrace",
-      },
-      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
-      {
-        name: "/r/battlestations",
-        url: "https://reddit.com/r/battlestations",
       },
       { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
       { name: "/r/news", url: "https://reddit.com/r/news" },
@@ -41,7 +32,6 @@ const bookmarks = [
     title: "Social",
     links: [
       { name: "Twitter", url: "https://twitter.com" },
-      { name: "Facebook", url: "https://facebook.com" },
     ],
   },
 ];
