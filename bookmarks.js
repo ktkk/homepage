6c1fc6cf0bf7eb36888b27ec94fe7f8c1d8fe033
katkak <62206630+ktkk@users.noreply.github.com>
2022-10-05 18:24:17 +0200

    Updated links

--- bookmarks.js
+++ bookmarks.js
@@ -1,39 +1,40 @@
 // Note: having length != 4 will mess with layout based on how the site is styled
 const bookmarks = [
   {
-    title: "Daily",
+    title: "Dev",
     links: [
-      { name: "Inbox", url: "https://inbox.google.com" },
-      { name: "GitHub", url: "https://github.com" },
-      { name: "Drive", url: "https://drive.google.com" },
-      { name: "Leho", url: "https://leho-howest.instructure.com/" },
+      { name: "GitHub", url: "https://github.com/" },
+      { name: "Crates.io", url: "https://crates.io/" },
+      { name: "Rust docs", url: "https://docs.rust-lang.org/" },
+      { name: "Rust playground", url: "https://play.rust-lang.org/" },
+      { name: "Go docs", url: "https://go.dev/doc/" },
+      { name: "Go playground", url: "https://go.dev/play/" },
+      { name: "C/C++ docs", url: "https://en.cppreference.com/w/" },
+      { name: "Godbolt compiler explorer", url: "https://godbolt.org/" },
     ],
   },
   {
     title: "Media",
     links: [
-      { name: "Youtube", url: "https://youtube.com" },
-	  { name: "Twitch", url: "https://www.twitch.tv/" },
-      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Youtube", url: "https://youtube.com/" },
+      { name: "Twitch", url: "https://www.twitch.tv/" },
+      { name: "Netflix", url: "https://netflix.com/" },
     ],
   },
   {
-    title: "Reddit",
+    title: "News",
     links: [
-      { name: "/r/all", url: "https://www.reddit.com/r/all/" },
-      {
-        name: "/r/pcmasterrace",
-        url: "https://reddit.com/r/pcmasterrace",
-      },
-      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
-      { name: "/r/news", url: "https://reddit.com/r/news" },
+      { name: "VRT NWS", url: "https://www.vrt.be/vrtnws/nl/" },
+      { name: "Lobsters", url: "https://lobste.rs/" },
+      { name: "Hackernews", url: "https://news.ycombinator.com/" },
     ],
   },
   {
     title: "Social",
     links: [
-      { name: "Twitter", url: "https://twitter.com" },
-      { name: "Discord", url: "https://discord.com/channels/@me" },
+      { name: "Twitter", url: "https://twitter.com/" },
+      { name: "Reddit", url: "https://reddit.com/" },
+      { name: "Discord", url: "https://discord.com/channels/@me/" },
     ],
   },
 ];
